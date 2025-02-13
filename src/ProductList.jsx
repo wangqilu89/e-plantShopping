3617854684330142d6b964156812acfb9dd18c45
Qilu <wangqilu.89@gmail.com>
2025-02-13 10:49:44 -0500

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+git add --aimport React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList() {
@@ -268,7 +268,21 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
+             {plantsArray.map((category, index) => (
+            <div key={index}>
+                <h1><div>{category.category}</div></h1>
+                <div className="product-list">
+                    {category.plants.map((plant, plantIndex) => (
+                    <div className="product-card" key={plantIndex}>
+                        <img className="product-image" src={plant.image} alt={plant.name} />
+                        <div className="product-title">{plant.name}</div>
+                        {/*Similarly like the above plant.name show other details like description and cost*/}
+                        <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                    </div>
+                    ))}
+                </div>
+            </div>
+            ))}
 
         </div>
  ) :  (
