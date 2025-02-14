1aa5c954cefa19ba07e8679ee5e68f96288af5c5
Wang Qilu <wangqilu.89@gmail.com>
2025-02-13 21:40:36 -0500

    jsdfka

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -29,7 +29,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   const handleDecrement = (item) => {
     ((item.quantity == 1)?dispatch(removeItem(item)):dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 })));
-    //dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    
   };
 
   const handleRemove = (item) => {
