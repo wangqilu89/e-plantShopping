c639fc7d70a332cd792bd7287e0abb38ec6a9b6c
Wang Qilu <wangqilu.89@gmail.com>
2025-02-13 20:00:02 -0500

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,4 @@
-git add --aimport React, { useState,useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import {addItem} from './CartSlice'
