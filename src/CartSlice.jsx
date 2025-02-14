2568d8b09d7a4f39785bc03ebb718d81b7e4512b
Wang Qilu <wangqilu.89@gmail.com>
2025-02-13 21:22:42 -0500

    test

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -16,6 +16,7 @@ export const CartSlice = createSlice({
         }
     },
     removeItem: (state, action) => {
+        console.log(action.payload)
         state.items = state.items.filter(item => item.name != action.payload);
     },
     updateQuantity: (state, action) => {
